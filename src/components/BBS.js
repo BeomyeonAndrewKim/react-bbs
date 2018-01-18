c017ab8c609692df5faeb0bbeea921de77f01a26
BeomyeonAndrewKim <beom911@gmail.com>
2018-01-18 14:53:25 +0900

    로그인 버튼 생성

--- src/components/BBS.js
+++ src/components/BBS.js
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
+import Login from './LoginScreen';
+import * as firebase from 'firebase';
 
 export default class BBS extends Component{
+  state = {
+    page:'login'
+  }
+  componentDidMount(){
+    const config = {
+      apiKey: "AIzaSyBzTPdE2X6jrR7K-pgJuMy_N1sLrSxBMY4",
+      authDomain: "react-bbs.firebaseapp.com",
+      databaseURL: "https://react-bbs.firebaseio.com",
+      projectId: "react-bbs",
+      storageBucket: "react-bbs.appspot.com",
+      messagingSenderId: "609470552804"
+    };
+    firebase.initializeApp(config);
+  }
   render(){
     return(
-      <div>BBS</div>
+      <div>
+        {
+          this.state.page==='login'
+          ? <Login/>
+          : null
+        }
+      </div>
     )
   }
 }
