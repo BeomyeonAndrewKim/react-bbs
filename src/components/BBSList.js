8251e7bf2477585404eac4494168008ea390ed49
BeomyeonAndrewKim <beom911@gmail.com>
2018-01-22 12:54:32 +0900

    게시글 목록 추가

--- src/components/BBSList.js
+++ src/components/BBSList.js
@@ -1,5 +1,30 @@
 import React,{Component} from 'react';
+import {Table} from 'semantic-ui-react';
+import styled from 'styled-components';
+
 import NavBar from './NavBar';
+
+const mockDate=[
+  {
+    articleID:'-LB1',
+    author:'andrew',
+    title: '게시글 제목 1',
+    createdAt: '2018-01-20'
+  },
+  {
+    articleID:'0d21',
+    author:'andrew',
+    title:'게시글 제목 2',
+    createdAt: '2018-01-12'
+  }
+]
+  
+const ArticleItemRow = styled(Table.Row)`
+  &:hover{
+    cursor:pointer;
+    background-color:#d5b6ef;
+  }
+`
 
 export default class BBSList extends Component{
   render(){
@@ -7,7 +32,26 @@ export default class BBSList extends Component{
     return(
       <div>
         <NavBar nickName={nickName} handleAccountScreen={handleAccountScreen}/>
-        <h1>BBSList</h1>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>작성자</Table.HeaderCell>
+              <Table.HeaderCell>제목</Table.HeaderCell>
+              <Table.HeaderCell>작성일</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {
+              mockDate.map(({articleID, title ,author, createdAt})=>(
+                <ArticleItemRow key={articleID}>
+                  <Table.Cell>{author}</Table.Cell>
+                  <Table.Cell>{title}</Table.Cell>
+                  <Table.Cell>{createdAt}</Table.Cell>
+                </ArticleItemRow>
+              ))
+            }
+          </Table.Body>
+        </Table>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 import BBS from './components/BBS';
 import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<BBS />, document.getElementById('root'));
 registerServiceWorker();
